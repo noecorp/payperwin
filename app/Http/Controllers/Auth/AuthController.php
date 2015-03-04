744e868c897e2b93d1d61fe17cd8c835e972a8a0
Nikita Logachev <nikita.logachev@me.com>
2015-03-04 21:15:41 +0100

    Auth controller making use of Socialite

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,37 +2,238 @@
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\Registrar;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Laravel\Socialite\Contracts\Factory as Socialite;
+use Illuminate\Http\Request;
+use App\Contracts\Repository\Users;
+use Illuminate\Contracts\View\Factory as View;
+use Illuminate\Routing\Redirector as Redirect;
 
 class AuthController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+	/**
+	 * The Guard implementation.
+	 *
+	 * @var Guard
+	 */
+	protected $auth;
 
-	use AuthenticatesAndRegistersUsers;
+	/**
+	 * The Users Repository implementation.
+	 *
+	 * @var Users
+	 */
+	protected $users;
+
+	/**
+	 * The Socialite Factory implementation.
+	 *
+	 * @var Socialite
+	 */
+	protected $socialite;
+
+	/**
+	 * The View Factory implementation.
+	 *
+	 * @var View
+	 */
+	protected $view;
+
+	/**
+	 * The Redirector implementation.
+	 *
+	 * @var Redirect
+	 */
+	protected $redirect;
+
 
 	/**
 	 * Create a new authentication controller instance.
 	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @param  Guard  $auth
+	 * @param  Users  $users
+	 * @param  Socialite  $socialite
+	 * @param  View  $view
+	 * @param  Redirect  $redirect
+	 *
 	 * @return void
 	 */
-	public function __construct(Guard $auth, Registrar $registrar)
+	public function __construct(Guard $auth, Users $users, Socialite $socialite, View $view, Redirect $redirect)
 	{
 		$this->auth = $auth;
-		$this->registrar = $registrar;
+		$this->users = $users;
+		$this->socialite = $socialite;
+		$this->view = $view;
+		$this->redirect = $redirect;
 
 		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+	/**
+	 * Show the application registration form.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getRegister()
+	{
+		return $this->view->make('auth.register');
+	}
+
+	/**
+	 * Handle a registration request for the application.
+	 *
+	 * @param  \App\Http\Requests\Register  $request
+	 * @param  Redirect  $redirect
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postRegister(\App\Http\Requests\Register $request)
+	{
+		$this->auth->login($this->users->create($request->all()));
+
+		return $this->redirect->to('/auth/register');
+	}
+
+	/**
+	 * Show the application login form.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getLogin()
+	{
+		return $this->view->make('auth.login');
+	}
+
+	/**
+	 * Handle a login request to the application.
+	 *
+	 * @param  \App\Http\Requests\Login  $request
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postLogin(\App\Http\Requests\Login $request)
+	{
+		$credentials = $request->only('email', 'password');
+
+		if ($this->auth->attempt($credentials, $request->has('remember')))
+		{
+			return $this->redirect->intended('/dashboard');
+		}
+
+		return $this->redirect->to('/auth/login')
+					->withInput($request->only('email', 'remember'))
+					->withErrors([
+						'email' => 'These credentials do not match our records.',
+					]);
+	}
+
+	/**
+	 * Log the user out of the application.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getLogout()
+	{
+		$this->auth->logout();
+
+		return $this->redirect->to('/');
+	}
+
+	/**
+	 * Redirects to appropriate social authentication site.
+	 *
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
+	public function getWith($provider)
+	{
+		return $this->socialite->with($provider)->redirect();
+	}
+
+	/**
+	 * Processes callback from social authentication site.
+	 *
+	 * @param  Request  $request
+	 * @param  string $provider
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getProvider(Request $request, $provider)
+	{
+		$error = $request->get('error');
+
+		if ($error)
+		{
+			return $this->redirectWithSocialError($error);
+		}
+		else
+		{
+			$user = $this->socialite->with($provider)->user();
+
+			$existing = ($provider == 'twitch') ? $this->users->withTwitchId($user->getId()) : $this->users->withFacebookId($user->getId());
+
+			if ($existing)
+			{
+				return $this->loginExisting($existing);
+			}
+			else
+			{
+				// What if the username is already taken in the system?
+
+				if ($provider == 'twitch')
+				{
+					$existing = $this->users->withUsername($user->getNickname());
+					
+					if ($existing)
+					{
+						 $user->nickname = null;
+					}
+				}
+
+				$newUser = $this->createWithSocial($provider, $user);
+
+				return $this->loginExisting($newUser, '/profile');
+			}
+		}
+	}
+
+	protected function redirectWithSocialError($error)
+	{
+		if ($error == 'access_denied') $error = 'We need the requested access to your profile to continue.';
+
+		return $this->redirect->to('auth/register')->withErrors(['social'=>$error]);
+	}
+
+	protected function loginExisting(\App\Models\User $user, $to = null)
+	{
+		$this->auth->login($user);
+
+		if ($to)
+			return redirect($to);
+
+		return $this->redirect->to('/dashboard');
+	}
+
+	protected function createWithSocial($provider, \Laravel\Socialite\Contracts\User $user)
+	{
+		$data = [
+			'username' => $user->getNickname(),
+			'email' => $user->getEmail(),
+		];
+
+		if ($provider == 'twitch')
+		{
+			$data['twitch_id'] = $user->getId();
+			$data['twitch_username'] = $user->getNickname();
+		}
+		else
+		{
+			$data['facebook_id'] = $user->getId();
+		}
+
+		return $this->users->create($data);
+
+		### -> flash something?
+		### -> redirect to choosing username and password
+		### -> if email null, account locked until email set
 	}
 
 }
