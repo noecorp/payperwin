d6796528797a8ae1d1db1381100f9f4544393677
Nikita Logachev <nikita.logachev@me.com>
2015-03-10 12:50:56 +0100

    building out pledges repository

--- app/Contracts/Repository/Pledges.php
+++ app/Contracts/Repository/Pledges.php
@@ -1,5 +1,13 @@
 <?php namespace App\Contracts\Repository;
 
 interface Pledges extends RepositoryContract {
+
+	public function havingIdWithStreamer($id);
+
+	public function latestWithUsersAndStreamers($take, $page = null);
+
+	public function latestForStreamerWithUsers($streamerId, $take, $page = null);
+
+	public function latestForUserWithStreamers($userId, $take, $page = null);
 	
 }
--- app/Repositories/Pledges.php
+++ app/Repositories/Pledges.php
@@ -3,11 +3,48 @@
 use App\Contracts\Repository\Pledges as PledgesRepository;
 use App\Models\Pledge;
 
-class Pledges implements PledgesRepository {
-	
-	public function create(array $data)
+class Pledges extends AbstractRepository implements PledgesRepository {
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param \App\Models\Pledge $pledge
+	 */
+	public function __construct(Pledge $pledge)
 	{
-		return Pledge::create($data);
+		parent::__construct($pledge);
+	}
+
+	public function latestWithUsersAndStreamers($take, $page = null)
+	{
+		$query = $this->newQuery()->with('owner','streamer');
+
+		$this->addLimits($query,$take,$page);
+
+		return $query->get();
+	}
+
+	public function latestForStreamerWithUsers($streamerId, $take, $page = null)
+	{
+		$query = $this->newQuery()->with('owner')->whereStreamerId($streamerId);
+
+		$this->addLimits($query,$take,$page);
+
+		return $query->get();
+	}
+
+	public function latestForUserWithStreamers($userId, $take, $page = null)
+	{
+		$query = $this->newQuery()->with('streamer')->whereUserId($userId);
+
+		$this->addLimits($query,$take,$page);
+
+		return $query->get();
+	}
+
+	public function havingIdWithStreamer($id)
+	{
+		return $this->newQuery()->with('streamer')->find($id);
 	}
 
 }
