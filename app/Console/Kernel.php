1dfc91a57509af8fc0873bd675533df359c74ed7
Nikita Logachev <nikita.logachev@me.com>
2015-03-24 00:29:09 +0100

    don't need artisan inspire command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,6 @@ class Kernel extends ConsoleKernel {
 	 * @var array
 	 */
 	protected $commands = [
-		'App\Console\Commands\Inspire',
 		'App\Console\Commands\Impress',
 		'App\Console\Commands\MakeTest',
 	];
@@ -24,8 +23,6 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('inspire')
-				 ->hourly();
 		$schedule->command('impress')
 				->everyFiveMinutes();
 	}
