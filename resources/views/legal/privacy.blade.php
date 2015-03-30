0cc57ac007b6c4deff38df05865560a5dc48c905
Nikita Logachev <nikita.logachev@me.com>
2015-03-31 00:35:37 +0200

    legal fillers

---
+++ resources/views/legal/privacy.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('title','Privacy Policy')
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<h1>Privacy Policy</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			
+		</div>
+	</div>
+@endsection
---
+++ resources/views/legal/terms.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('title','Terms &amp; Conditions')
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<h1>Terms &amp; Conditions</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			
+		</div>
+	</div>
+@endsection
