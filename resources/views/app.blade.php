ab0660ce7880985d6efb9b4eb639c37af061da98
Nikita Logachev <nikita.logachev@me.com>
2015-03-31 00:36:13 +0200

    fuller templating and styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,24 +27,29 @@
 	<nav class="navbar navbar-inverse navbar-lg navbar-fixed-top" role="navigation">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-7">
-			<span class="sr-only">Toggle navigation</span>
-		  </button>
-		  <a class="navbar-brand" href="/">PayPerWin</a>
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-7">
+				<span class="sr-only">Toggle navigation</span>
+			</button>
+			<a class="navbar-brand" href="/">PayPerWin</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="navbar-collapse-7">
-		  <ul class="nav navbar-nav">
-			<li><a href="/streamers">Streamers {{ ($streamersLiveNow) ? '('.$streamersLiveNow.' live)' : '' }}</a></li>
-		   </ul>
-		  <ul class="nav navbar-nav navbar-right">
+			<ul class="nav navbar-nav">
+				<li><a href="/streamers">Streamers {{ ($streamersLiveNow) ? '('.$streamersLiveNow.' live)' : '' }}</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
 			@if ($auth->guest())
 				<li><a href="/auth/login" >Login</a></li>
 				<li><a href="/auth/register" >Register</a></li>
 			@else
+				@if ($auth->user()->streamer)
+					<li><a href="/earnings/{{ $auth->user()->id }}">Earnings: ${{ round($auth->user()->earnings,2) }}</a></li>
+				@else
+					<li><a href="/transactions/{{ $auth->user()->id }}">Funds: ${{ round($auth->user()->funds,2) }}</a></li>
+				@endif
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ $auth->user()->username }} <b class="caret"></b></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset($auth->user()->avatar) }}" class="avatar"> {{ $auth->user()->username }} <b class="caret"></b></a>
 					<ul class="dropdown-menu">
 						<li><a href="/users/{{ $auth->user()->id }}">Profile</a></li>
 						<li><a href="/users/{{ $auth->user()->id }}/edit">Settings</a></li>
@@ -62,15 +67,25 @@
 		</div><!-- /.navbar-collapse -->
 	</nav>
 
-	<div class="container">
+	<article class="container">
 		<div class="row">
 			<div class="col-xs-12">
 				@yield('content')
 			</div>
 		</div>
-	</div>
+	</article>
 
 	<footer>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/streamers">Streamers</a></li>
+			<li><a href="mailto:payperwin@helpful.io" data-helpful="payperwin" data-helpful-modal="on">Contact Us</a></li>
+			<li><a href="/privacy">Privacy Policy</a></li>
+			<li><a href="/terms">Terms &amp; Conditions</a></li>
+		</ul>
+		<p>
+			<small>PayPerWin isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</small>
+		</p>
 	</footer>
 
 	<div id="helpful">
