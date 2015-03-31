c88c490a68651fff2fb252db140c8885190666ca
Nikita Logachev <nikita.logachev@me.com>
2015-03-31 23:55:45 +0200

    experimenting with intercom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -98,5 +98,16 @@
 		<a href="mailto:payperwin@helpful.io" data-helpful="payperwin" data-helpful-modal="on" class="btn btn-info">Help &amp; Feedback</a>
 	</div>
 
+	@if ($auth->user())
+		<script>
+			window.intercomSettings = {
+				name: "{{ $auth->user()->username }}",
+				email: "{{ $auth->user()->email }}",
+				created_at: "{{ $auth->user()->created_at->timestamp }}",
+				app_id: "{{ (env('APP_ENV') == 'production') ? "w0ju1yb6" : "egqks52q" }}"
+			};
+		</script>
+		<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/w0ju1yb6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+	@endif
 </body>
 </html>
