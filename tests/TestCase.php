ccd0b3420de7be1a78d7049a93fd468f3e5ed27e
Nikita Logachev <nikita.logachev@me.com>
2015-04-05 18:21:22 +0200

    can test if response is a view

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -35,6 +35,11 @@ class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 		return PHPUnit::assertNotNull(json_decode($this->response->getContent()));
 	}
 
+	public function assertResponseIsView($response)
+	{
+		return PHPUnit::assertTrue(is_object($response) && isset($response->original) && $response->original instanceof \Illuminate\View\View);
+	}
+
 	public function responseJson()
 	{
 		return json_decode($this->response->getContent());
