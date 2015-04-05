e570158988c97b02c109fe375fc0a9979db0b17d
Nikita Logachev <nikita.logachev@me.com>
2015-04-05 16:41:27 +0200

    basic UserPledges functional testing

---
+++ tests/Functional/Controllers/UsersPledgesTest.php
@@ -0,0 +1,28 @@
+<?php namespace AppTests\Functional\Controllers;
+
+class UsersPledgesTest extends \AppTests\TestCase {
+
+	public function testIndexOkWithNoPledges()
+	{
+		$response = $this->call('GET','users/1/pledges');
+
+		$this->assertResponseOk();
+		$this->assertViewHas('pledges');
+	}
+
+	public function testIndexOkWithPledges()
+	{
+		$response = $this->call('GET','users/2/pledges');
+
+		$this->assertResponseOk();
+		$this->assertViewHas('pledges');
+	}
+
+	public function testIndexAbortsWhenNotFound()
+	{
+		$response = $this->call('GET','users/foo/pledges');
+
+		$this->assertResponseStatus(404);
+	}
+
+}
