9cf7d1ec37a69aa144dbf66a0fbad5e0eb6ebc0b
Nik <nikita.logachev@me.com>
2015-04-27 01:03:44 +0200

    no intercom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -126,17 +126,5 @@
 			<small>PayPerWin isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</small>
 		</p>
 	</footer>
-	
-	@if ($auth->user())
-		<script>
-			window.intercomSettings = {
-				name: "{{ $auth->user()->username }}",
-				email: "{{ $auth->user()->email }}",
-				created_at: "{{ $auth->user()->created_at->timestamp }}",
-				app_id: "{{ (env('APP_ENV') == 'production') ? "w0ju1yb6" : "egqks52q" }}"
-			};
-		</script>
-		<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/w0ju1yb6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-	@endif
 </body>
 </html>
